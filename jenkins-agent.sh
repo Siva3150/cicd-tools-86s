97158d7c41570d1dda25e05a6f6098adec137b6d
Siva prasad <sivaprasad3150@gmail.com>
2026-01-03 13:02:50 +0530

    jenkins

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -46,3 +46,8 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
 #k9s
 sudo curl -sS https://webinstall.dev/k9s | bash
+
+#helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
